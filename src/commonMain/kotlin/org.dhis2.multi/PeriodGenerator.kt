6dadc6594ee1df80a65885d4a039110ee91d619d
Enrico <enrico@dhis2.org>
2022-10-26 15:30:56 +0200

    Add periodType enum

--- src/commonMain/kotlin/org.dhis2.multi/PeriodGenerator.kt
+++ src/commonMain/kotlin/org.dhis2.multi/PeriodGenerator.kt
@@ -1,17 +1,17 @@
 package org.dhis2.multi
 
 import kotlinx.datetime.LocalDate
+import org.dhis2.multi.PeriodType.*
 
 class PeriodGenerator {
 
-    fun getPeriodId(date: LocalDate, periodType: String): Period {
+    fun getPeriodId(date: LocalDate, periodType: PeriodType): Period {
         return when (periodType) {
-            "Yearly" -> YearlyPeriodGenerator().getPeriodId(date)
-            "Six_monthly" -> SixMonthPeriodGenerator().getPeriodId(date)
-            "Quarterly" -> QuarterPeriodGenerator().getPeriodId(date)
-            "Monthly" -> MonthPeriodGenerator().getPeriodId(date)
-            "Daily" -> DailyPeriodGenerator().getPeriodId(date)
-            else -> Period("")
+            Yearly -> YearlyPeriodGenerator().getPeriodId(date)
+            Six_monthly -> SixMonthPeriodGenerator().getPeriodId(date)
+            Quarterly -> QuarterPeriodGenerator().getPeriodId(date)
+            Monthly -> MonthPeriodGenerator().getPeriodId(date)
+            Daily -> DailyPeriodGenerator().getPeriodId(date)
         }
     }
 }
---
+++ src/commonMain/kotlin/org.dhis2.multi/PeriodType.kt
@@ -0,0 +1,9 @@
+package org.dhis2.multi
+
+enum class PeriodType {
+    Yearly,
+    Six_monthly,
+    Quarterly,
+    Monthly,
+    Daily
+}
